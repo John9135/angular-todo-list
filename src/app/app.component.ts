17d788a1ad6ad258564a16228f38d2cf298b31a9
John9135 <palacioalexander5@gmail.com>
2023-07-31 12:01:17 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from './item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+
+  //toDo : Delete this list the moment of integer
+  filter: "all" | "activate" | "done" = "all"
+  allItems = [
+    { description: "dormir", done: true },
+    { description: "ir a la cama", done: false },
+    { description: "Lavar ropa", done: false },
+    { description: "ir al doctor", done: false },
+    { description: "ir al odontologo", done: false },
+  ]
+
+  get items() {
+    if (this.filter === "all") {
+      return this.allItems
+    }
+    return this.allItems.filter((item) => {
+      this.filter === "done" ? item.done : !item.done
+    })
+  }
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    })
+  }
+
+  removeItem(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1)
+  }
 }
+
+
