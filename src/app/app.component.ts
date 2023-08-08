15c2b317e7ec585175c06f311d084dbfc4e78214
John9135 <palacioalexander5@gmail.com>
2023-08-08 11:03:09 -0500

    feat: Create filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { Item } from './item';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'todo';
 
   //toDo : Delete this list the moment of integer
-  filter: "all" | "activate" | "done" = "all"
+  filter: "all" | "active" | "done" = "all";
   allItems = [
     { description: "dormir", done: true },
     { description: "ir a la cama", done: false },
--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, Input, EventEmitter } from '@angular/core';
+import {Item} from '../item';
 
 @Component({
   selector: 'app-item',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent {
+  editable = false;
 
+  @Input() item!: Item;
+  @Output() remove = new EventEmitter<Item>();
+
+  saveItem(description: string) {
+    if (!description) return;
+    this.editable = false;
+    this.item.description = description;
+  }
 }
